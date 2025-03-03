e19630b92e3edc45b21317844d3e42c327ee51be
Johann Verlohner <verlonerjohann@gmail.com>
2025-03-03 10:21:57 -0800

    updating todos

--- zig_testing/guessing-game.zig
+++ zig_testing/guessing-game.zig
@@ -2,14 +2,16 @@ const std = @import("std");
 const rand = std.crypto.random;
 
 pub fn main() !void {
-    while (true) {
-        const stdin = std.io.getStdIn().reader();
-        const stdout = std.io.getStdOut().writer();
+    const stdin = std.io.getStdIn().reader();
+    const stdout = std.io.getStdOut().writer();
+    const number = rand.intRangeLessThan(u8, 0, 255);
 
-        const number = rand.intRangeAtMost(u8, 0, 255);
+    while (true) {
+        try stdout.writeAll("Guess a number between 0 and 254: ");
+
         const bare_line = try stdin.readUntilDelimiterAlloc(std.heap.page_allocator, '\n', 8000);
         defer std.heap.page_allocator.free(bare_line);
-        const line = std.mem.trim(u8, bare_line, "\r");
+        const line = std.mem.trim(u8, bare_line, &[_]u8{'\r'});
 
         const guess = std.fmt.parseInt(u8, line, 10) catch |err| switch (err) {
             error.Overflow => {
@@ -21,9 +23,12 @@ pub fn main() !void {
                 continue;
             },
         };
-        if (guess < number) try stdout.writeAll("Too small\n");
-        if (guess > number) try stdout.writeAll("Too big\n");
-        if (guess == number) {
+
+        if (guess < number) {
+            try stdout.writeAll("Too small\n");
+        } else if (guess > number) {
+            try stdout.writeAll("Too big\n");
+        } else {
             try stdout.writeAll("Correct!\n");
             break;
         }
